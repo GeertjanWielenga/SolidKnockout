8001a179462780120e75ef4ae6c019eb2c35aabe
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-03 15:50:09 +0200

    including jquery

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -3,6 +3,7 @@ require.config({
     paths: {
         knockout: './bower_components/knockout/dist/knockout',
         text: './bower_components/requirejs-text/text',
+        jquery: './bower_components/jquery/dist/jquery',
         initComponents: './js/initComponents'
     },
     waitSeconds: 2
