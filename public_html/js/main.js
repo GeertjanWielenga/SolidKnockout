d15ff7e554ab67d04818959b0a208d3f07bce776
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-07 13:28:27 +0200

    including response.js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,12 +4,31 @@ require.config({
         knockout: './js/libraries/knockout/dist/knockout',
         text: './js/libraries/requirejs-text/text',
         jquery: './js/libraries/jquery/dist/jquery',
+        response: './js/libraries/responsejs/response',
         initComponents: './js/initComponents'
+    },
+    // Shim configuration for modules that do not expose AMD:
+    shim : {
+        'response':{
+            deps: ['jquery']
+        }  
     },
     waitSeconds: 2
 });
-require(['knockout', 'initComponents'],
+require(['knockout', 'initComponents', 'response'],
     function (ko, initComponents) {
+        var self = this;
+        self.weAreSmall = ko.observable();
+        Response.create({
+            breakpoints: [0, 480, 481]
+        });
+        Response.action(function() {
+             if (Response.band(0, 480)){
+                 self.weAreSmall(true);
+             } else {
+                 self.weAreSmall(false);
+             }
+        });
         ko.applyBindings(new initComponents());
     }
 );
